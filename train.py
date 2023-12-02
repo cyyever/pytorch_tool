5a50b297eda341db9b7b4dac6ce5e34b6417bc47
cyy <cyyever@outlook.com>
2023-12-02 12:24:26 +0800

    Add code

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import datetime
 import os
 
+import cyy_torch_graph  # noqa: F401
+import cyy_torch_text  # noqa: F401
 import hydra
 from cyy_naive_lib.log import add_file_handler
 from cyy_torch_toolbox.default_config import Config
